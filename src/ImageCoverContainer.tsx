02fe58bb7478d05e993299fb80a895036b5b6ea9
Hadar <hadarhadar888@gmail.com>
2023-03-05 09:19:23 +0200

    create select story items screen

--- src/ImageCoverContainer.tsx
+++ src/ImageCoverContainer.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import BookImage from "./BookImage";
 import loader from './loader.json';
 import headers from './headers';
+import { IconButton } from "@mui/material";
+import {NavigateNext} from '@mui/icons-material';
 
 interface ImageCoverContainerProps {
     mainCharacterAnimal: string,
@@ -12,23 +14,27 @@ interface ImageCoverContainerProps {
 
 const ImageCoverContainer = (props: ImageCoverContainerProps) => {
     const {mainCharacterAnimal, storyLocation} = props;
-    const [coverImage, setCoverImage] = useState();
-
-    const imageParams = {
-        prompt: `${mainCharacterAnimal} in ${storyLocation} as drawing art for kids`,
-        model: 'image-alpha-001'
-    };
+    const [coverImage, setCoverImage] = useState<string>();
 
     useEffect(() => {
-        axios.post('https://api.openai.com/v1/images/generations', imageParams, { headers: headers })
-            .then(response => {
-                console.log(response.data);
-                setCoverImage(response.data.data[0].url);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, [])
+        const imageParams = {
+            prompt: `${mainCharacterAnimal} in ${storyLocation} as drawing art for kids`,
+            model: 'image-alpha-001'
+        };
+
+        // axios.post('https://api.openai.com/v1/images/generations', imageParams, { headers: headers })
+        //     .then(response => {
+        //         console.log(response.data);
+        //         setCoverImage(response.data.data[0].url);
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
+
+        const response = {data: [{url: 'https://www.eckersleys.com.au/media/wysiwyg/blog/Projects/Images/How_to_draw/How_to_draw_dog_colour_Preview.jpg'}]}
+        setCoverImage(response.data[0].url);
+
+    }, [mainCharacterAnimal, storyLocation])
 
     return (
         <>
@@ -36,6 +42,9 @@ const ImageCoverContainer = (props: ImageCoverContainerProps) => {
                 coverImage ? <BookImage bookImageUrl={coverImage} imageText={"sdsds"} isCoverImage /> :
                     <Lottie animationData={loader} loop={true} />
             }
+            <IconButton aria-label="next">
+                <NavigateNextIcon />
+            </IconButton>
         </>
     )
 }
